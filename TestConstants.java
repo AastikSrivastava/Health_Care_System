7f200c18dddd3b7c6e9d2455cf2a6f5bf3d83887
AastikSrivastava <48197367+AastikSrivastava@users.noreply.github.com>
2020-09-22 23:37:35 +0530

    Add files via upload

---
+++ TestConstants.java
@@ -0,0 +1,21 @@
+package org.com.capg.healthcare.util;
+
+public class TestConstants {
+
+	
+	public static final String TEST_ADDED="Test added successfully for center ";
+	
+	public static final String TEST_NOT_FOUND="Test not found ";
+	
+	public static final String CENTER_NOT_FOUND="Center not found";
+	
+	public static final String ADD_TEST_URL= "addtest/{tid}/{cid}";
+	
+	public static final String VIEW_TEST_BY_CENTER_URL="viewtest/{cid}";
+	
+	public static final String VIEW_CENTER_BY_TEST="viewcenters/{tid}";
+
+	public static final String NAME_ALREADY_EXIST = "Id already exist";
+	
+	public static final String ADD_TEST_URL_BY_ID="addtest";
+}
